f6e6715943ffb4daf365be258962ac24cb09c021
echoboncu <ekosaputra14@guru.smk.belajar.id>
2026-03-03 07:46:17 +0700

    First Commit

---
+++ operator.php
@@ -0,0 +1,12 @@
+<?php
+
+// Belajar Operator
+
+$nilai1 = 10;
+$nilai2 = 20;
+
+// + - / * %
+
+$penjumlahan = $nilai1 + $nilai2;
+echo "<b>" . $penjumlahan . "</b><br>";
+// output 30
---
+++ variable.php
@@ -0,0 +1,10 @@
+<?php
+// Belajar Variable
+$namadepan = "Eko Saputra";
+echo $namadepan;
+echo $namadepan;
+echo $namadepan;
+echo $namadepan;
+echo $namadepan;
+echo $namadepan;
+echo $namadepan;
